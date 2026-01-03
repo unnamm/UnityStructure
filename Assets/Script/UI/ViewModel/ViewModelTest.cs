85a0a19b9c460f0aac0d1d2f70729e7bb1ff3b0b
ljs <ljs85633@gmail.com>
2026-01-03 22:11:11 +0900

    package appui

---
+++ Assets/Script/UI/ViewModel/ViewModelTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.AppUI.MVVM;
+
+namespace Assets.Script.UI.ViewModel
+{
+    internal class ViewModelTest : ObservableObject
+    {
+    }
+}
